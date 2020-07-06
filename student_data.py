1816b7d80e57c9718ced792e71def36a7e189bcc
marwai <mtse@spartaglobal.com>
2020-07-06 10:19:22 +0100

    inheritance student

---
+++ student_data.py
@@ -0,0 +1,14 @@
+# The super class, Student is the class that other classes inherits from
+class Student:
+    # method (behaviour) initialises the variables
+    def __init__(self, name, age, course): # (name, age, course attributes are defined)
+        self.name = name
+        self.__age = age
+        self.course = course
+
+    # Details of the student
+    def details(self):
+        # basic print functions
+        print("Name: " + self.name)
+        print("Age: " + self.__age)
+        print("University course: " + self.course)
